23bbc781eb8f7bfe2bca1fbb49cbb98e802f1386
Divija Naredla <divija.naredla@sjsu.edu>
2022-02-25 15:09:38 -0800

    updated server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Working as expected!');
+  res.end('Hello, This is my 281 first lab assignment!');
 }
 
 const server = http.createServer(requestListener);
 server.listen(8080);
-console.log('Server running successfully');
+console.log('Connect and running successfully');
